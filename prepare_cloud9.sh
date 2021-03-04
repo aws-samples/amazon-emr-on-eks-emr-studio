21356b64480bda88e164c6280b2dfe5273be2c4c
tanmatth <79859357+tanmatth@users.noreply.github.com>
2021-03-04 10:16:50 -0500

    Create prepare_cloud9.sh

---
+++ prepare_cloud9.sh
@@ -0,0 +1,39 @@
+### Preparing Cloud9
+
+## installing jq
+sudo yum -y install jq
+
+## Installing kubectl
+sudo curl --silent --location -o /usr/local/bin/kubectl \
+   https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.11/2020-09-18/bin/linux/amd64/kubectl
+
+sudo chmod +x /usr/local/bin/kubectl
+
+sudo pip install --upgrade awscli && hash -r
+
+echo 'yq() {
+  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
+}' | tee -a ~/.bashrc && source ~/.bashrc
+
+for command in kubectl jq envsubst aws
+  do
+    which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
+  done
+
+kubectl completion bash >>  ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
+## Installing eksctl
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv -v /tmp/eksctl /usr/local/bin
+
+eksctl version
+
+eksctl completion bash >> ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
+
